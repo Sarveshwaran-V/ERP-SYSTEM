80a948dc4ed3e57ab20dd73d2f708d0ffe53f078
Sarveshwaran-V <sarveshwaran1089@gmail.com>
2024-07-24 22:41:15 +0530

    database updated

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://kollege-api.onrender.com",
-    // "http://localhost:3500",
+   "http://localhost:3500",
     //? this is the nodejs port, which is 3500 by default, and not the react port(3000).
 
   headers: { "Content-Type": "application/json" },
